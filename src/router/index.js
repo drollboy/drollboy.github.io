0709d88fc370f752b6e11d85498cbc0cadcfba4c
drollboy <2660695548@qq.com>
2025-03-21 16:20:51 +0800

    解决路由问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
 
 import HomePage from '../views/HomePage.vue';
 import ChatPage from '../views/ChatPage.vue';
@@ -7,7 +7,7 @@ const routes = [
   { path: '/chat', component: ChatPage },
 ];
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 export default router;
