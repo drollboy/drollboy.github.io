b2ec592748082814615cfe7fbdf81944e21063ab
drollboy <2660695548@qq.com>
2025-03-21 16:14:15 +0800

    修复路由问题

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    historyApiFallback: true, // 配置服务器支持HTML5 History API
+  }
 })
